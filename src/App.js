3f5a7cd8769e769fcffa3ddffcf37c3b871c8614
Remzi <ict@remzi.info>
2019-10-05 14:19:17 +0200

    Added keys

--- src/App.js
+++ src/App.js
@@ -7,24 +7,28 @@ class ComputerModels extends Component {
   state = {
     data: [
       {
+        id: 1,
         name: "Ivel Z3",
         manufacturer: "Ivasim",
         year: 1969,
         origin: "Croatia"
       },
       {
+        id: 2,
         name: "Bally Astrocade",
         manufacturer: "Bally Consumer Products",
         year: 1977,
         origin: "USA"
       },
       {
+        id: 3,
         name: "Sord M200 Smart Home Computer",
         manufacturer: "Sord Computer Corporation",
         year: 1971,
         origin: "Japan"
       },
       {
+        id: 4,
         name: "Commodore 64",
         manufacturer: "Commodore",
         year: 1982,
@@ -40,7 +44,7 @@ class ComputerModels extends Component {
     return <select>
       <option value="">-- pick a model --</option>
       { models.map(model => {
-        return <option value={model.name}>{model.name} ({model.year})</option>
+        return <option key={model.id} value={model.name}>{model.name} ({model.year})</option>
         }) }
     </select>
   }
